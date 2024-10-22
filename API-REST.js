7d7153a1aa8fb94798256baa5a0e827f7ad96025
LayGomez <laylamelanygomez@gmail.com>
2024-10-22 10:28:00 +0200

    refactor: add const baseAPi

--- API-REST.js
+++ API-REST.js
@@ -1,27 +1,23 @@
+const baseAPI = "https://6716056f33bc2bfe40bc0567.mockapi.io/items";
 export async function getItemsFromApi() {
-  const response = await fetch(
-    "https://6716056f33bc2bfe40bc0567.mockapi.io/items"
-  );
+  const response = await fetch(baseAPI);
   const itemsData = await response.json();
   return itemsData;
 }
 
 export async function createDataApi(newItem) {
-  const response = await fetch(
-    "https://6716056f33bc2bfe40bc0567.mockapi.io/items",
-    {
-      method: "POST",
+  const response = await fetch(baseAPI, {
+    method: "POST",
 
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(newItem),
-    }
-  );
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(newItem),
+  });
   const newItemAPI = await response.json();
   return newItemAPI;
 }
 
 export async function deleteDataFromApi(itemId) {
-  await fetch("https://6716056f33bc2bfe40bc0567.mockapi.io/items/" + itemId, {
+  await fetch(baseAPI + "/" + itemId, {
     method: "DELETE",
   });
 }
